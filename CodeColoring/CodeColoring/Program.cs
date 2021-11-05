939a527a7308bbf31e8ae8d21e39a7b2f9b70ff8
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2021-11-05 22:33:51 +0500

    Summary (required)

---
+++ CodeColoring/CodeColoring/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeColoring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
