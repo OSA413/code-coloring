a1e4b7c5075b5762c6d32faa939bc79af496269d
MehLater <zavyalovartem79@gmail.com>
2021-11-17 14:29:37 +0500

    Revert "Основа Colorize"
    This reverts commit c60417791c95a8925b5b5c616e0a460e0d2be7a7.

--- CodeColoring/CodeColoring/Colorizer/Colorizer.cs
+++ CodeColoring/CodeColoring/Colorizer/Colorizer.cs
@@ -3,49 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Drawing;
 
 namespace CodeColoring.Colorizer
 {
-    public class Colorizer : ColorPalette
+    class Colorizer
     {
-        private List<ColorizedArgument> result = new List<ColorizedArgument>();
-        public IEnumerable<ColorizedArgument> Colorize(ParseResult parseResult)
-        {
-            //Предположу что в парс резалте будет словарь - аргумент:тип
-            foreach (var arg in parseResult.results)
-            {
-                switch (arg.LanguageUnit)
-                {
-                    case LanguageUnit.Function:
-                        result.Add(new ColorizedArgument(this.FunctionColor, arg.arg));
-                        break;
-                    case LanguageUnit.FunctionDefinition:
-                        result.Add(new ColorizedArgument(this.FunctionDefinitionColor, arg.arg));
-                        break;
-                    case LanguageUnit.Operator:
-                        result.Add(new ColorizedArgument(this.OperatorColor, arg.arg));
-                        break;
-                    case LanguageUnit.Symbols:
-                        result.Add(new ColorizedArgument(this.SymbolColor, arg.arg));
-                        break;
-                    case LanguageUnit.Variable:
-                        result.Add(new ColorizedArgument(this.VariableColor, arg.arg));
-                }
-            }
-            return result;
-        }
-    }
-
-    public class ColorizedArgument
-    {
-        public Color ArgumentColor { get; set; }
-        public string Argument { get; set; }
-
-        public ColorizedArgument(Color color, string argument)
-        {
-            ArgumentColor = color;
-            Argument = argument;
-        }
+        
     }
 }
