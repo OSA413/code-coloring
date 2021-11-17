c60417791c95a8925b5b5c616e0a460e0d2be7a7
MehLater <zavyalovartem79@gmail.com>
2021-11-17 14:27:51 +0500

    Основа Colorize

--- CodeColoring/CodeColoring/Colorizer/ColorPalette.cs
+++ CodeColoring/CodeColoring/Colorizer/ColorPalette.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace CodeColoring
 {
     public abstract class ColorPalette
     {
-
+        public Color SymbolColor { get; set; }
+        public Color FunctionDefinitionColor { get; set; }
+        public Color VariableColor { get; set; }
+        public Color FunctionColor { get; set; }
+        public Color OperatorColor { get; set; }
     }
 }
