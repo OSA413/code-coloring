987702ae31538c69e1822b6b849efac506fc2a2e
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2021-11-20 22:36:16 +0500

    ooops

--- CodeColoring/CodeColoring/Program.cs
+++ CodeColoring/CodeColoring/Program.cs
@@ -6,7 +6,7 @@ namespace CodeColoring
 {
     public class Program
     {
-        public void Main(string[] args)
+        public static void Main(string[] args)
         {
             var dargs = Repository.Kernel.Get<IArgsDecoder>().Decode(args);
             var inputText = Repository.Kernel.Get<StreamReader>().ReadToEnd();
