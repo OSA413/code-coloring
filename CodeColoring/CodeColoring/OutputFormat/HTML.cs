7c6df17e49cc514aebf16404a93a0e57cf6d4289
alenmaz <hideseyes@gmail.com>
2021-11-22 18:02:13 +0500

    слияние форматтера с солорайзером

--- CodeColoring/CodeColoring/OutputFormat/HTML.cs
+++ CodeColoring/CodeColoring/OutputFormat/HTML.cs
@@ -3,63 +3,89 @@ using System.Text;
 using System.Linq;
 using CodeColoring.Colorizer;
 using CodeColoring.ProgrammingLanguage;
+using System.Drawing;
 
 namespace CodeColoring.OutputFormat
 {
     public class HTML : IOutputFormat
     {
-        private Colorizer.Colorizer colorizer;
-        public HTML(Colorizer.Colorizer colorizer) => this.colorizer = colorizer;
-        public string Format(ParsingResult pu, ColorPalette palette)
+        HTMLPageSettings pageSettings = HTMLPageSettings.DefaultSettings();
+        public string Format(ParsingResult parsed, ColorPalette palette)
         {
             var result = new StringBuilder();
-            var title = "Colored code";
-            var tab = 4;
+            pageSettings.Palette = palette;
 
             result.Append("<!DOCTYPE HTML>\n");
             result.Append("<html>\n");
 
-            result.Append(FormatHeader(title, tab));
-            result.Append(FormatBody(pu, palette, tab));
+            result.Append(FormatHeader(pageSettings.Palette,  
+                pageSettings.Title, 
+                pageSettings.BackgroundColor, 
+                pageSettings.Font, 
+                pageSettings.FontSize, 
+                pageSettings.TabSize));
+            result.Append(FormatBody(parsed, pageSettings.TabSize));
 
             result.Append("</html>\n");
             return result.ToString();
         }
 
-        private string FormatBody(ParsingResult pu, ColorPalette palette, int tab)
+        private string FormatBody(ParsingResult parsed, int tab)
         {
             var body = new StringBuilder();
-            ParseUnit unit;
-            int space = 1;
-
             body.Append(new String(' ', tab) + "<body>\n");
             body.Append(new String(' ', tab * 2) + "<code>" + "<pre>\n");
-            for(int i = 0; i < pu.Result.Count; i++)
-            {
-                unit = pu.Result[i];
-                if (i < pu.Result.Count - 1)
-                    space = unit.Symbol.Equals("(") || pu.Result[i + 1].Symbol.Any(c => !Char.IsLetterOrDigit(c)) ? 0 : 1;
-                body.Append(FormatUnit(unit, palette) + new String(' ', space));
-            }
+            foreach(var unit in parsed.Result)
+                body.Append(FormatUnit(unit));
             body.Append(new String(' ', tab * 2) + "\n</code>" + "</pre>\n");
             body.Append(new String(' ', tab) + "</body>\n");
             return body.ToString();
         }
 
-        private string FormatHeader(string pageTitle, int tab)
+        private string FormatHeader(ColorPalette palette, string pageTitle, Color backColor, string font, int size, int tab)
         {
             var header = new StringBuilder();
             header.Append(new String(' ', tab) + "<head>\n");
                 header.Append(new String(' ', tab * 2) + $"<title>{pageTitle}</title>\n");
                 header.Append(new String(' ', tab * 2) + "<meta charset=\"UTF-8\">\n");
+                header.Append(new String(' ', tab * 2) + "<style>\n");
+                    header.Append(new String(' ', tab * 3) + $"body {{background-color: rgb({backColor.R},{backColor.G},{backColor.B}); font-family: {font}; font-size: {size};}}\n");
+                    header.Append(new String(' ', tab * 3) + $".Function {{color: rgb({palette.FunctionColor.R},{palette.FunctionColor.G},{palette.FunctionColor.B});}}\n");
+                    header.Append(new String(' ', tab * 3) + $".Comment {{color: rgb({palette.CommentColor.R},{palette.CommentColor.G},{palette.CommentColor.B});}}\n");
+                    header.Append(new String(' ', tab * 3) + $".FunctionDefinition {{color: rgb({palette.FunctionDefinitionColor.R},{palette.FunctionDefinitionColor.G},{palette.FunctionDefinitionColor.B});}}\n");
+                    header.Append(new String(' ', tab * 3) + $".Operator {{color: rgb({palette.OperatorColor.R},{palette.OperatorColor.G},{palette.OperatorColor.B});}}\n");
+                    header.Append(new String(' ', tab * 3) + $".Symbol {{color: rgb({palette.SymbolColor.R},{palette.SymbolColor.G},{palette.SymbolColor.B});}}\n");
+                    header.Append(new String(' ', tab * 3) + $".Variable {{color: rgb({palette.VariableColor.R},{palette.VariableColor.G},{palette.VariableColor.B});}}\n");
+                    header.Append(new String(' ', tab * 3) + $".Value {{color: rgb({palette.ValueColor.R},{palette.ValueColor.G},{palette.ValueColor.B});}}\n");
+                    header.Append(new String(' ', tab * 3) + $".Whitespace {{color: rgb({palette.WhitespaceColor.R},{palette.WhitespaceColor.G},{palette.WhitespaceColor.B});}}\n");
+                    header.Append(new String(' ', tab * 3) + $".Unknown {{color: rgb({palette.UnknownColor.R},{palette.UnknownColor.G},{palette.UnknownColor.B});}}\n");
+                header.Append(new String(' ', tab * 2) + "</style>\n");
             header.Append(new String(' ', tab) + "</head>\n");
             return header.ToString();
         }
 
-        private string FormatUnit(ParseUnit unit, ColorPalette palette)
+        private string FormatUnit(ParseUnit unit) => $"<span class=\"{unit.Unit}\">{unit.Symbol}</span>";
+    }
+
+    public class HTMLPageSettings
+    {
+        public string Title { get; }
+        public string Font { get; }
+        public int FontSize { get; }
+        public Color BackgroundColor { get; }
+        public int TabSize { get; }
+        public ColorPalette Palette { get; set; }
+
+        public static HTMLPageSettings DefaultSettings() => new HTMLPageSettings("Colored code", "serif", 14, Color.White, 4, new DayTheme());
+
+        public HTMLPageSettings(string title, string font, int fontSize, Color backgroundColor, int tabSize, ColorPalette palette)
         {
-            var color = colorizer.Colorize(unit.Unit, palette);
-            return $"<span style=\"color:rgb({color.R},{color.G},{color.B})\">{unit.Symbol}</span>";
+            Title = title;
+            Font = font;
+            FontSize = fontSize;
+            BackgroundColor = backgroundColor;
+            TabSize = tabSize;
+            Palette = palette;
         }
     }
 }
